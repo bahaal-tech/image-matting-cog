6b0a8f4e4fa29d98a94a0344269e62730e68dc74
iamargentum <rajat.rj97@gmail.com>
2023-12-19 14:38:27 +0530

    Support matting with trimap as well as mask

--- predict.py
+++ predict.py
@@ -1,9 +1,15 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
+import cv2
+import numpy as np
 from pymatting import cutout
-from cog import BasePredictor, Input, Path
+from cog import BasePredictor, BaseModel, Input, Path
 
+class MattingOutput(BaseModel):
+    segmentedImage: Path
+    success: bool
+    error: str
 
 class Predictor(BasePredictor):
     def setup(self) -> None:
@@ -11,9 +17,42 @@ class Predictor(BasePredictor):
 
     def predict(
         self,
-        image: Path = Input(description="Grayscale input image"),
-        trimap: Path = Input(description="Trimap image"),
-    ) -> Path:
-        cutout(image, trimap, "output.png")
+        image: Path = Input(description="Input image"),
+        mask: Path = Input(description="Mask image", default=None),
+        trimap: Path = Input(description="Trimap image", default=None),
+    ) -> MattingOutput:
+        # if there's no mask/trimap, return an error
+        if mask is None and trimap is None:
+            return MattingOutput(segmentedImage=None, success=False, error="Must provide either mask or trimap")
+        
+        # if mask is present, create trimap using mask and then cut out the image
+        if mask is not None:
+            kernel = np.ones((10, 10), np.uint8)
 
-        return Path("output.png")
+            maskImage = cv2.imread(mask)
+            erodedImage = cv2.erode(maskImage, kernel, iterations=1)
+            dilatedImage = cv2.dilate(maskImage, kernel, iterations=1)
+
+            newImage = np.zeros((erodedImage.shape[0], erodedImage.shape[1]), np.uint8)
+
+
+            for i in range(0, erodedImage.shape[0]):
+                for j in range(0, erodedImage.shape[1]):
+                    erosionPixel = erodedImage[i][j]
+                    dilationPixel = dilatedImage[i][j]
+
+                    if erosionPixel > 0:
+                        newImage[i][j] = 255
+                    elif dilationPixel > 0:
+                        newImage[i][j] = 127
+            
+            cv2.imwrite("trimap.png", newImage)
+
+            cutout(image, "trimap.png", "output.png")
+        
+        else:
+            # if execution reaches here, trimap must be present, thus
+            # cut the image using the trimap
+            cutout(image, trimap, "output.png")
+
+        return MattingOutput(segmentedImage=Path("output.png"), success=True, error=None)
