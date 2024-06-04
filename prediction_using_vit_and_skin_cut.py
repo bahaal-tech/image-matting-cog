fba0aeba2e3bd817f55910a9a9d404c53766a6cf
Anustup Mukherjee <60361231+Anustup900@users.noreply.github.com>
2024-06-04 15:12:01 +0530

    change

--- prediction_using_vit_and_skin_cut.py
+++ prediction_using_vit_and_skin_cut.py
@@ -48,7 +48,7 @@ class SkinSegmentVitMatte:
             return {"success": False, "error": f"matting modifications failed due to:{modified_matte['error']}",
                     "vit_matte_path": edge_less_matte_mask_path}
         modified_matte_image = cv2.imread(modified_matte["output"])
-        kernel_for_modified_matte = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+        kernel_for_modified_matte = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
         final_image = cv2.morphologyEx(modified_matte_image, cv2.MORPH_OPEN, kernel_for_modified_matte, iterations=3)
         dir_final = os.path.join(DIRECTORY_TO_SAVE_MODIFIED_MATTE, "final_matte_image.png")
         cv2.imwrite(dir_final, final_image)
